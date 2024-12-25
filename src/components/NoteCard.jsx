292b4ebb05b10a88e082e4616171465d8316ce4f
birtukan <birtukandegu2127@gmail.com>
2024-12-25 11:17:00 +0300

    refactor(state): Centralize note state with React Context for better accessibility

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -7,7 +7,7 @@ import { db } from "../appwrite/databases";
 import Spinner from '../icons/Spinner';
 import DeleteButton from './DeleteButton';
 
-const NoteCard = ({ note, setNotes }) => {
+const NoteCard = ({ note }) => {
     const [saving, setSaving] = useState(false);
     const keyUpTimer = useRef(null);
 
@@ -101,7 +101,7 @@ const NoteCard = ({ note, setNotes }) => {
                 className="card-header" 
                 style={{backgroundColor: colors.colorHeader}}
             >
-                <DeleteButton setNotes={setNotes} noteId={note.$id}/>
+                <DeleteButton noteId={note.$id}/>
 
                 {saving && (
                     <div className="card-saving">
